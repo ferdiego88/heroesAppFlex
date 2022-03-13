9ee03f1339ed3eaa2239d2483543833a946375f9
Fernando Santillan <ferdiego88@gmail.com>
2022-03-13 08:26:42 -0500

    mostrar Rutas Hijas

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
 
 const routes: Routes = [
   {
-    path: '', children: [
+    path: '',
+    component: HomeComponent,
+     children: [
       { path: 'listado', component: ListadoComponent},
       { path: 'agregar', component: AgregarComponent},
       { path: 'editar/:id', component: AgregarComponent},
