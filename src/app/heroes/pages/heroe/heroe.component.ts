55c419c6b76efacccc2b2289d2f8bc6388193fbf
Fernando Santillan <ferdiego88@gmail.com>
2022-04-12 20:17:11 -0500

    boton Regresar

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { switchMap,tap } from 'rxjs/operators';
 import { Heroe } from '../../interfaces/heroes.interfaces';
 import { HeroesService } from '../../services/heroes.service';
@@ -14,7 +14,8 @@ export class HeroeComponent implements OnInit {
   heroe!: Heroe;
 
   constructor(private activatedRoute: ActivatedRoute,
-              private heroeService: HeroesService) {
+              private heroeService: HeroesService,
+              private router: Router) {
   
    }
 
@@ -27,6 +28,10 @@ export class HeroeComponent implements OnInit {
       .subscribe(heroe => {
         this.heroe = heroe;
       })
+  }
+
+  regresar() {
+    this.router.navigate(['/heroes/listado']);
   }
 
 }
