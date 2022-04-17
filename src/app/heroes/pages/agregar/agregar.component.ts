d42e13a1b4dff27ac9724ec7a4a03913d1962ad1
Fernando Santillan <ferdiego88@gmail.com>
2022-04-17 15:48:42 -0500

    diseño Agregar Heroe

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe, Publisher } from '../../interfaces/heroes.interfaces';
 
 @Component({
   selector: 'app-agregar',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarComponent implements OnInit {
 
+  heroe: Heroe = {
+    superhero: '',
+    alter_ego: '',
+    characters: '',
+    first_appearance: '',
+    publisher: Publisher.DCComics
+  }
+
+  publishers = [
+    {id: 'DC Comics',
+     desc: 'DC - COMICS'
+    },
+     {id: 'Marvel Comics',
+     desc: 'MARVEL - COMICS'
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  guardar() {
+    if (this.heroe.superhero.trim().length===0) {
+      return;
+    }
+    console.log(this.heroe);
   }
 
 }
